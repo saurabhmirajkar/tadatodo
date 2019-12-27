36428b095734fcf8ad096879265b2825ae33e34f
Saurabh Mirajkar <mirajkar46@gmail.com>
2019-12-27 19:34:47 +0530

    Add data model

--- TaDaToDo/Controller/ToDoViewController.swift
+++ TaDaToDo/Controller/ToDoViewController.swift
@@ -7,16 +7,58 @@
 //
 
 import UIKit
+import CoreData
 
 class ToDoViewController: UIViewController {
     
+    @IBOutlet weak var todoTableView: UITableView!
     
+    var itemsArray = [Item]()
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        todoTableView.dataSource = self
+        todoTableView.delegate = self
     }
 
+    @IBAction func addButtonPressed(_ sender: UIButton) {
+        var textField = UITextField()
+        let alert = UIAlertController(title: "Add New Todoey Item", message: "", preferredStyle: .alert)
+        let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
+            
+            let newItem = Item()
+            newItem.title = textField.text!
+            self.itemsArray.append(newItem)
+            self.todoTableView.reloadData()
+            /*let newItem = Item(context: self.context)
+            newItem.title = textField.text!
+            newItem.done = false
+            newItem.parentCategory = self.selectedCategory
+            self.itemArray.append(newItem)
+            self.saveItems()*/
+        }
+        
+        alert.addTextField { (alertTextField) in
+            alertTextField.placeholder = "Create new item"
+            textField = alertTextField
+        }
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
+    }
 
 }
 
+extension ToDoViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemsArray.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "todoCell", for: indexPath) as! ToDoTableViewCell
+        cell.textLabel?.text = itemsArray[indexPath.row]
+        return cell
+    }
+    
+}
---
+++ TaDaToDo/View/ToDoTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ToDoTableViewCell.swift
+//  TaDaToDo
+//
+//  Created by Saurabh Mirajkar on 27/12/19.
+//  Copyright © 2019 Saurabh Mirajkar. All rights reserved.
+//
+
+import UIKit
+
+class ToDoTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
